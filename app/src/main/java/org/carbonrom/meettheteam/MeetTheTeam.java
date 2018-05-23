be3faeae6b771fddd0581f298bcb251344e94097
Adin Kwok <adin.kwok@carbonrom.org>
2018-05-23 10:19:14 -0700

    Add owners/leads/core

---
+++ app/src/main/java/org/carbonrom/meettheteam/MeetTheTeam.java
@@ -0,0 +1,13 @@
+package org.carbonrom.meettheteam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MeetTheTeam extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.meet_the_team);
+    }
+}
